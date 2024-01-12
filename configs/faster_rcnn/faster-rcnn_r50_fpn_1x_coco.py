7a67647df1ab106caf61f22d898d57d8a4d8aa9b
klinsc <klinscea@gmail.com>
2024-01-12 13:53:24 +0700

    added: asdr6-3-o-split8020-1280

--- configs/faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
     "../_base_/models/faster-rcnn_r50_fpn.py",
-    "../_base_/datasets/asdr6-3-o-split8020-640.py",
-    "../_base_/schedules/schedule_1x.py",
+    "../_base_/datasets/asdr6-3-o-split8020-1280.py",
+    "../_base_/schedules/schedule_2x.py",
     "../_base_/default_runtime.py",
 ]
