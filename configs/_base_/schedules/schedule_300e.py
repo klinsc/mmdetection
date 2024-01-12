8e5b833bd93043d9f59885bbe01d3fe03ac188ea
klinsc <klinscea@gmail.com>
2024-01-12 15:58:22 +0700

    Update schedule_300e.py

--- configs/_base_/schedules/schedule_300e.py
+++ configs/_base_/schedules/schedule_300e.py
@@ -1,5 +1,5 @@
-# training schedule for 20e
-train_cfg = dict(type="EpochBasedTrainLoop", max_epochs=20, val_interval=1)
+# training schedule for 300e
+train_cfg = dict(type="EpochBasedTrainLoop", max_epochs=300, val_interval=1)
 val_cfg = dict(type="ValLoop")
 test_cfg = dict(type="TestLoop")
 
