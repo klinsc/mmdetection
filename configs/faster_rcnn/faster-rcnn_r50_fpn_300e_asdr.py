e4370cb5bea89e9e9b64764f7b9d6a4604347455
klinsc <klinscea@gmail.com>
2024-01-14 17:14:56 +0700

    Update faster-rcnn_r50_fpn_300e_asdr.py

--- configs/faster_rcnn/faster-rcnn_r50_fpn_300e_asdr.py
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_300e_asdr.py
@@ -1,6 +1,6 @@
 _base_ = [
     "../_base_/models/faster-rcnn_r50_fpn.py",
     "../_base_/datasets/asdr6-3-1000oa-split8020-1280.py",
-    "../_base_/schedules/schedule_300e.py",
+    "../_base_/schedules/schedule_20e.py",
     "../_base_/default_runtime.py",
 ]
