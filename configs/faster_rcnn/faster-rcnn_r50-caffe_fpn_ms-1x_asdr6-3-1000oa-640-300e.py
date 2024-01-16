c85081a86f2b159037563dbb45dea8edc6f18a0b
klinsc <klinscea@gmail.com>
2024-01-16 20:03:49 +0700

    Create faster-rcnn_r50-caffe_fpn_ms-1x_asdr6-3-1000oa-640-300e.py

---
+++ configs/faster_rcnn/faster-rcnn_r50-caffe_fpn_ms-1x_asdr6-3-1000oa-640-300e.py
@@ -0,0 +1,93 @@
+_base_ = "./faster-rcnn_r50-caffe_fpn_ms-1x_coco.py"
+
+class_name = (
+    "11522_tx_dyn1",
+    "11522_tx_ynyn0d1",
+    "115_1way_ds_w_motor",
+    "115_3ways_ds_w_motor",
+    "115_breaker",
+    "115_buffer",
+    "115_cvt_1p",
+    "115_cvt_3p",
+    "115_ds",
+    "115_gs",
+    "115_gs_w_motor",
+    "115_la",
+    "115_vt_1p",
+    "115_vt_3p",
+    "22_breaker",
+    "22_cap_bank",
+    "22_ds",
+    "22_ds_la_out",
+    "22_gs",
+    "22_ll",
+    "22_vt_3p",
+    "BCU",
+    "DIM",
+    "DPM",
+    "LL",
+    "MU",
+    "NGR_future",
+    "Q",
+    "remote_io_module",
+    "ss_man_mode",
+    "tele_protection",
+    "terminator_double",
+    "terminator_single",
+    "terminator_splicing_kits",
+    "terminator_w_future",
+    "v_m",
+    "v_m_digital",
+)
+num_classes = len(class_name)
+palette = [
+    (0, 0, 0),
+    (66, 0, 75),
+    (120, 0, 137),
+    (130, 0, 147),
+    (105, 0, 156),
+    (30, 0, 166),
+    (0, 0, 187),
+    (0, 0, 215),
+    (0, 52, 221),
+    (0, 119, 221),
+    (0, 137, 221),
+    (0, 154, 215),
+    (0, 164, 187),
+    (0, 170, 162),
+    (0, 170, 143),
+    (0, 164, 90),
+    (0, 154, 15),
+    (0, 168, 0),
+    (0, 186, 0),
+    (0, 205, 0),
+    (0, 224, 0),
+    (0, 243, 0),
+    (41, 255, 0),
+    (145, 255, 0),
+    (203, 249, 0),
+    (232, 239, 0),
+    (245, 222, 0),
+    (255, 204, 0),
+    (255, 175, 0),
+    (255, 136, 0),
+    (255, 51, 0),
+    (247, 0, 0),
+    (228, 0, 0),
+    (215, 0, 0),
+    (205, 0, 0),
+    (204, 90, 90),
+    (204, 204, 204),
+]
+
+model = dict(roi_head=dict(bbox_head=dict(num_classes=num_classes)))
+metainfo = {
+    "classes": class_name,
+    "palette": palette,
+}
+
+train_dataloader = dict(dataset=dict(metainfo=metainfo))
+val_dataloader = dict(dataset=dict(metainfo=metainfo))
+test_dataloader = val_dataloader
+
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_bbox_mAP-0.398_20200504_163323-30042637.pth"  # noqa
